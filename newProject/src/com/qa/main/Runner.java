aa7dacb00a9bb55b5863a936ae534918cd96f049
Christian <christian.balazs@yahoo.com>
2021-10-19 10:49:00 +0100

    first commit of Java project

---
+++ newProject/src/com/qa/main/Runner.java
@@ -0,0 +1,16 @@
+package com.qa.main;
+
+public class Runner {
+
+	// Main method
+	// ctrl space opens auto complete
+	
+	public static void main(String[] args) {
+		// All code runs from Main top to bottom
+		
+		System.out.println("Hello everyone!");
+		// syso ctrl space -> System out
+	}
+	
+	
+}
