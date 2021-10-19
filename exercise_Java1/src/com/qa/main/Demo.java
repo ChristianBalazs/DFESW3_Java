a9dbf0a73c0c91c8a716e0b6326cdd9f61baf303
Christian <christian.balazs@yahoo.com>
2021-10-19 11:50:09 +0100

    commit for Java exercise

---
+++ exercise_Java1/src/com/qa/main/Demo.java
@@ -0,0 +1,17 @@
+package com.qa.main;
+
+public class Demo {
+
+	public static void favPizza() {
+		System.out.println("fav Pizza Topping is ketchup");
+	}
+	
+	public static void favWord() {
+		System.out.println("Fav word is learn");
+	}
+	
+	public static void favNumber() {
+		System.out.println("Fav number is 10");
+	}
+	
+}
---
+++ exercise_Java1/src/com/qa/main/Main.java
@@ -0,0 +1,19 @@
+package com.qa.main;
+
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+
+	Demo.favPizza();
+	
+	Demo.favWord();
+	
+	Demo.favNumber();
+	
+	}
+	
+	
+	
+}
