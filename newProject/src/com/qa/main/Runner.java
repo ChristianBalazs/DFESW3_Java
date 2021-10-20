ab96e09b6b6d4e68162082f5c9b74342baf5999b
Christian <christian.balazs@yahoo.com>
2021-10-20 12:39:03 +0100

    RE: changes to newProject

--- newProject/src/com/qa/main/Runner.java
+++ newProject/src/com/qa/main/Runner.java
@@ -6,11 +6,51 @@ public class Runner {
 	// ctrl space opens auto complete
 	
 	public static void main(String[] args) {
-		// All code runs from Main top to bottom
 		
-		System.out.println("Hello everyone!");
-		// syso ctrl space -> System out
+		// All code runs from Main top to bottom
+		//System.out.println("3");
+		//System.out.println("2");
+		//System.out.println("1");
+		
+		
+		// Don't write code in Main, link to methods
+		
+		// Tell my main method to sun newMethod
+		// type in the name of the method to run + () + ;
+		
+		//newMethod();
+		//helloDeacon();
+		
+		
+		// Reference the class . the method
+		// Demo.imFirst();
+		
+		Stack.firstMethod();
+		Stack.secondMethod();
+		
+				
+		System.out.println(MethodStack.firstLayer());
+		
+		
 	}
+	
+	
+	// New method
+	// All methods should be public static void (but thsi will change)
+	
+	//public static void newMethod() {
+		// method body - what runs when we say "run this method"
+		
+		//System.out.println("Hello world!");
+		// syso ctrl space -> System out
+	//}
+	
+	// make a new method called helloDeacon
+	
+	//public static void helloDeacon() {
+	//	System.out.println("Hello Deacon !");
+	//}
+	
 	
 	
 }
